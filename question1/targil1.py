a711ec195303e4c74ae8eb6fa7c44208bed3e7a3
rina4365 <rgrin4365@gmail.com>
2025-04-08 17:07:17 +0300

    question 1

--- question1/targil1.py
+++ question1/targil1.py
@@ -3,32 +3,34 @@ import os
 
 
 errors_list = dict()
-err_pattern = re.compile(r'\bERR_\w+\b')
 
 
 
-def process(chunk):
-    lines = chunk.splitlines(True)
-    for line in lines:
-        err_type_line = re.search(err_pattern,line)
-        if err_type_line:
-            if err_type_line.group(0) in errors_list.keys():
-                errors_list[err_type_line.group(0)] += 1
-            else:
-                errors_list[err_type_line.group(0)] = 1
-
+def process(line):
+    split_line = line.split()  
+    if len(split_line) != 5:
+        return
+    if split_line[4] in errors_list.keys():
+        errors_list[split_line[4]] += 1
+    else:
+        errors_list[split_line[4]] = 1
 
 
 def readFile(file_path):
-    chunk_size = 1024
+    chunk_size = 1024*1024
     file = open(file_path, "r")
+    buffer=''
+
     while True:
-        data = file.read(1024*1024)
+        data = file.read(chunk_size)
         if not data:
             break
-        process(data) 
+        buffer += data
+        lines = buffer.split('\n')
+        for line in lines[:-1]:
+            process(line)
+        buffer = lines[-1]
     
-
 
 def main():
     file_path = r"C:\Users\User\Desktop\Hadasim\question1\logste.txt.txt"
@@ -40,12 +42,12 @@ def main():
     
     readFile(file_path)
 
-    x = int(input("Enter a number: "))
-    if x > len(errors_list):
-        x = len(errors_list)
+    n = int(input("Enter a number: "))
+    if n > len(errors_list):
+        n = len(errors_list)
 
     sorted_errors_list =dict(sorted(errors_list.items(), key=lambda x: x[1],reverse = True))
-    for key in list(sorted_errors_list)[:x]:
+    for key in list(sorted_errors_list)[:n]:
         print(key, sorted_errors_list[key])
     
 
